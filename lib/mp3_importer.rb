8a1781f572a012bbbace36f32b58d1ba92b19efd
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-06-19 21:23:09 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,7 +4,7 @@ class MP3Importer
   def initialize(title)
     @title = title
   end
-  
+
   def import(list_of_filenames)
     list_of_filenames.each {|filename| Song.new_by_filename(filename)}
   end
