89faa23c7c858ebc3792bd37a0c8b99e1d0fec48
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-06-19 21:35:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,19 @@
 class Song
   attr_accessor :name, :artist
-  @@all = []
+
   def initialize(name)
     @name = name
-    @@all << self
+  end
+
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
 
   def artist_name
     self.artist.name
-  end
-
-  def self.new_by_filename(filename)
-    song = self.new
-    song.title = filename.split(" - ")[1]
-    song
   end
 
   def self.all
