1a79e5d7330068b68d01e964281adff78c2a2131
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-06-19 21:36:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,11 +12,8 @@ class Song
     new_song
   end
 
-  def artist_name
-    self.artist.name
-  end
-
-  def self.all
-    @@all
+  def artist_name(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 end
