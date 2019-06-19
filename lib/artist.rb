2e191617877ad8092cf035e9d0a6c5ac98fbff80
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-06-19 21:33:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,19 +5,17 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
   end
 
   def add_song(song)
     @songs << song
-    song.artist = self
   end
 
   def songs
     @songs
   end
 
-  def self.save
+  def self.all
     @@all
   end
 end
