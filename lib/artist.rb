f5dd40a72c5c78db1e53d61df3b94912da93990d
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-06-19 21:16:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,15 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   @@all = []
   def initialize(name)
     @name = name
   end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
   def self.save
     @@all
   end
